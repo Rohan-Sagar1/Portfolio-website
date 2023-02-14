62e1996746df218a05b6d3c8971d9389e47ed736
Rohan Sagar <sagarrohan2002@gmail.com>
2023-02-14 04:23:43 -0500

    API updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import styled from "styled-components";
-import axios from "axios";
-import cheerio from "cheerio";
 import UFCTimer from "./UFCTimer";
 
 let id = 0;
@@ -14,32 +12,13 @@ const INITIAL_STATES = [
 ]
 
 function Home(props) {
-  const [tags, setTags] = useState(INITIAL_STATES);
   const [count, setCount] = useState(2);
-  const [event, setEvent] = useState([]);
   const [selectedChoice, setSelectedChoice] = useState("");
 
   const handleSelection = choice => {
     if (!selectedChoice) {
       setSelectedChoice(choice);
-    }
-  };
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const result = await axios.get('https://www.ufc.com/events');
-  //     const $ = cheerio.load(result.data);
-  //     const eventFighters = $('.c-card-event--result__info');
-  //     const events = [];
-  //     let event = $(eventFighters[0]).text().trim();
-  //     event = event.replace(/\s+/g, '');
-  //     const [name, date] = event.split('\n').map(item => item.trim());
-  //     events.push(name, date);
-  //     events.push(event);
-  //     setEvent(event);
-  //     console.log(event);
-  //   };
-  //   fetchData();
-  // }, []);
+    }};
 
   useEffect(() => {
     let interval = null;
@@ -55,85 +34,118 @@ function Home(props) {
 
   return (
     <Container>
-      <LeftMenu>
-        <TagWrapper>
-          {INITIAL_STATES.map(({ id, label}) => (
-            <Tag key={id}>{label}</Tag>
-          ))}
-        </TagWrapper>
-        <Name>ROHAN SAGAR</Name>
-        <SchoolDetails>
-          Junior at Purdue University
-          <br/>
-          05-03-2024 (MM-DD-YYYY)
-          </SchoolDetails>
-        <NumberChart>
-          <OtherInfo>
-            <h1>4</h1>
-            <span>SPOKEN <br/>LANGUAGES</span>
-          </OtherInfo>
-          <label></label>
+      <Landing>
+        <LeftMenu>
+          <TagWrapper>
+            {INITIAL_STATES.map(({ id, label}) => (
+              <Tag key={id}>{label}</Tag>
+            ))}
+          </TagWrapper>
+          <Name>ROHAN SAGAR</Name>
+          <SchoolDetails>Junior at Purdue University</SchoolDetails>
+          <Graduation>20-05-24 (DD-MM-YY)</Graduation>
+          <NumberChart>
+            <OtherInfo>
+              <h1>4</h1>
+              <span>SPOKEN <br/>LANGUAGES</span>
+            </OtherInfo>
+            <label></label>
 
-          <OtherInfo>
-            <h1>8</h1>
-            <span>PROGRAMMING<br/>LANGUAGES</span>
-          </OtherInfo>
-          <label></label>
+            <OtherInfo>
+              <h1>8</h1>
+              <span>PROGRAMMING<br/>LANGUAGES</span>
+            </OtherInfo>
+            <label></label>
 
-          <OtherInfo>
-            <h1>{count}</h1>
-            <span>COFFEE<br/>CUPS</span>
-          </OtherInfo>
-        </NumberChart>
-      </LeftMenu>
+            <OtherInfo>
+              <h1>{count}</h1>
+              <span>COFFEE<br/>CUPS</span>
+            </OtherInfo>
+          </NumberChart>
+        </LeftMenu>
 
-      <Picture src={"/assets/IMG_2224.png"} alt="me"></Picture>
-      <RightMenu>
-        <UFCTimer/>
-        <span>Sat, Feb 18 / 7:00 PM EST / Main Card</span>
-        <span>What are your picks for the fight?</span>
-        <div>
-        <div onClick={() => handleSelection("Red")}>
-          <p>Choice 1: Red</p>
-          {selectedChoice === "Red" && <p>Selected</p>}
-        </div>
-        <div onClick={() => handleSelection("Blue")}>
-          <p>Choice 2: Blue</p>
-          {selectedChoice === "Blue" && <p>Selected</p>}
-        </div>
-      </div>
+        <Picture>
+          <img src={"/assets/IMG_2224.png"} alt="me"></img>
+        </Picture>
 
-      </RightMenu>
-
+        <RightMenu>
+          <UFCTimer/>
+            <UserSelect>
+              <span>What are your picks for the fight?</span>
+              <RedOption onClick={() => handleSelection("Red")}>
+                <p>Red</p>
+                {selectedChoice === "Red"}
+              </RedOption>
+              <BlueOption onClick={() => handleSelection("Blue")}>
+                <p>Blue</p>
+                {selectedChoice === "Blue"}
+              </BlueOption>
+              <label>You selected: {selectedChoice}</label>
+            </UserSelect>
+        </RightMenu>
+      </Landing>
     </Container>
   )
 }
 
 export default Home
 
-
 const Container = styled.main`
   display: flex;
   width: 100%;
-  height: 84%;
+  height: 85%;
   position: absolute;
   background-color: #191A20;
-`
+  justify-content: center;
+  align-items: center;
 
-const Picture = styled.img`
-  height: 350px;
-  position: absolute;
-  bottom: 50px;
-  right: 45%;
-`
+  @media (max-width: 768px) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+`;
 
+const Landing = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  margin-top: 10%;
+  margin-left: 2%;
+`
 
 const LeftMenu = styled.div`
   display: flex;
   flex-direction: column;
-  position: relative;
-  top: 30%;
-  left: 5%;
+  gap: 10px;
+
+  @media (max-width: 768px) {
+    position: static;
+    text-align: center;
+    margin-bottom: 50px;
+  }
+`;
+
+const Picture = styled.div`
+  flex-direction: column;
+  img {
+    height: 350px;
+    border-radius: 6px;
+  }
+
+  @media (max-width: 768px) {
+    position: static;
+    margin-top: 50px;
+  }
+`;
+
+const RightMenu = styled.div`
+  color: white;
+  display: flex;
+  flex-direction: column;
+  margin-top: 40px;
+  width: 35%;
+  margin-right: 25px;
   gap: 10px;
 `
 
@@ -150,19 +162,30 @@ const Tag = styled.p`
   margin-right: 5px;
   border-radius: 3px;
   box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.15), 0px 2px 2px rgba(0, 0, 0, 0.15), 0px 3px 3px rgba(0, 0, 0, 0.15);
+  white-space: nowrap;
 `;
 
 const Name = styled.span`
   display: flex;
   color: white;
   font-size: 60px;
-`
+
+  @media (max-width: 768px) {
+    font-size: 36px;
+  }
+`;
 
 const SchoolDetails = styled.span`
   display: flex;
   color: white;
   font-size: 20px;
+  @media (max-width: 768px) {
+    font-size: 14px;
+  }
+`;
 
+const Graduation = styled(SchoolDetails)`
+  font-size: 14px;
 `
 
 const OtherInfo = styled.div`
@@ -180,9 +203,18 @@ const OtherInfo = styled.div`
   }
 
   span {
+    font-size: 18px;
+  }
+
+  @media (max-width: 768px) {
+  h1 {
+    font-size: 36px;
+  }
+
+  span {
     font-size: 12px;
   }
-`;
+}`;
 
 const NumberChart = styled.div`
   display: flex;
@@ -193,16 +225,67 @@ const NumberChart = styled.div`
     height: 90%;
   }
 `
-const RightMenu = styled.div`
-  color: white;
+
+const Date = styled.span`
   display: flex;
-  flex-direction: column;
-  position: absolute;
-  top: 42%;
-  transform: translateY(-50%);
-  right: 0;
-  margin-right: 20px;
-  gap: 10px;
+  font-size: 18px;
+  text-align: center;
+  margin-left: 100px;
 `
 
+const UserSelect = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+  margin-left: 120px;
+  gap: 20px;
+  justify-content: center;
 
+  span {
+    font-size: 18px;
+  }
+
+  label {
+    display: flex;
+    margin-left: 30px;
+  }
+`
+
+  const RedOption = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100px;
+  height: 40px;
+  margin-left: 50px;
+  background-color: rgb(143, 2, 14);
+  color: white;
+  font-family: "Share Tech Mono", monospace;
+  letter-spacing: 2px;
+  text-transform: uppercase;
+  cursor: pointer;
+  transition: all 0.2s;
+  user-select: none;
+  &:active {
+    transform: translateY(1px);
+  }
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.2);
+    transition: background-color 0.2s;
+    z-index: -1;
+  }
+  &:active::before {
+    background-color: rgba(0, 0, 0, 0.4);
+  }
+  `;
+
+  const BlueOption = styled(RedOption)`
+  background-color: rgb(41, 71, 144) !important;
+  `
--- src/components/UFCTimer.js
+++ src/components/UFCTimer.js
@@ -1,8 +1,29 @@
-import React, { useState, useEffect } from 'react'
-import styled from 'styled-components'
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import axios from "axios";
+import cheerio from "cheerio";
 
 function UFCTimer() {
   const [timeLeft, setTimeLeft] = useState(300);
+  const [event, setEvent] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const events = [];
+      const result = await axios.get('https://www.ufc.com/events');
+      const $ = cheerio.load(result.data);
+      const eventFighters = $('.c-card-event--result__headline');
+      const eventDate = $('.c-card-event--result__date');
+      let event = $(eventFighters[0]).text().trim();
+      let eventTime = $(eventDate[0]).text().trim();
+      const [firstFighter, secondFighter] = event.split(' vs ');
+      events.push(firstFighter);
+      events.push(secondFighter);
+      events.push(eventTime);
+      setEvent(events);
+    };
+    fetchData();
+  }, []);
 
   useEffect(() => {
     const countdown = setInterval(() => {
@@ -32,6 +53,7 @@ function UFCTimer() {
     }
 
   return (
+    <>
     <Timer>
       <Clock>
         <Info>
@@ -47,7 +69,7 @@ function UFCTimer() {
     <Contestant>
       <Container>
         <TextWrapper>
-            <span>Jones</span>
+            <span>{event[0]}</span>
         </TextWrapper>
         <RedBanner/>
       </Container>
@@ -55,16 +77,71 @@ function UFCTimer() {
     <ContestantTwo>
       <Container>
         <TextWrapper>
-            <span>Gane</span>
+            <span>{event[1]}</span>
         </TextWrapper>
         <BlueBanner/>
       </Container>
     </ContestantTwo>
   </Timer>
-  )
-}
+  <Date>{event[2]}</Date>
+  </>
+  )}
 
 export default UFCTimer
+
+const Clock = styled.div`
+  z-index: 1;
+  box-sizing: border-box;
+  width: 200px;
+  height: 100%;
+  padding: 0 1.5em;
+  background-image: linear-gradient(180deg,#363636,#2b2b2b);
+  transform-origin: center 0;
+  transform: translate(0px, 0%);
+  height: 100%;
+  display: flex;
+`
+const Logo = styled.div`
+    width: 69px;
+    transform: translate(0%, -50%);
+    left: 0px;
+    position: absolute;
+    top: 50%;
+    z-index: 1;
+    display: flex;
+    justify-content: center;
+    overflow: hidden;
+    img {
+      transform: translate(-10px, 0%);
+      width: 3.5em;
+      height: 100%;
+      transition: fill .3s ease;
+      filter: invert(100%);
+    }
+`
+
+const ClockWrapper = styled.div`
+    position: relative;
+    width: 100%;
+    height: 100%;
+    span{
+      opacity: 1;
+      visibility: inherit;
+      transform: translate(0%, -50%);
+      position: absolute;
+      right: 0;
+      top: 50%;
+      display: flex;
+      align-items: center;
+      margin-left: 0.4em;
+      color: white;
+      font-size: 1.4em;
+      font-family: 'Hanken Grotesk', sans-serif;
+      font-family: 'Quantico', sans-serif;
+      user-select: none;
+      display: flex;
+    }
+`
 
 const Info = styled.div`
   transform: translate(-50%, 0%);
@@ -92,31 +169,29 @@ const Info = styled.div`
     white-space: nowrap;
     text-transform: uppercase;
   }
-
 `
 
 const RedBanner = styled.span`
-    position: absolute;
-    top: 50%;
-    right: 0;
-    height: 20px;
-    width: 8px;
-    opacity: 1;
-    visibility: inherit;
-    transform: translate(0%, -50%);
-    background-color: rgb(143, 2, 14);
+  position: absolute;
+  top: 50%;
+  right: 0;
+  height: 20px;
+  width: 8px;
+  opacity: 1;
+  visibility: inherit;
+  transform: translate(0%, -50%);
+  background-color: rgb(143, 2, 14);
 `
 
 const BlueBanner = styled(RedBanner) `
-    background-color: rgb(41, 71, 144) !important;
-    transform: translate(0%, -50%);
-    right: auto;
-    left: 0;
+  background-color: rgb(41, 71, 144) !important;
+  transform: translate(0%, -50%);
+  right: auto;
+  left: 0;
 `
 const Contestant = styled.div`
   order: -1;
   overflow: hidden;
-  // margin-top: 150px;
   color: white;
   font-size: 1.3em;
   font-family: "Share Tech Mono",monospace;
@@ -135,106 +210,55 @@ const ContestantTwo = styled(Contestant)`
   flex-direction: row;
 `
 
-
 const TextWrapper = styled.div`
-    opacity: 1;
-    visibility: inherit;
-    transform: translate(0%, 0px);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    white-space: nowrap;
-    word-spacing: -0.25em;
-    color: white;
-    font-size: 0.9em;
-    font-family: "Share Tech Mono", monospace;
-    letter-spacing: 2px;
-    text-transform: uppercase;
-    -webkit-font-smoothing: antialiased;
-    font-weight: 500;
+  opacity: 1;
+  visibility: inherit;
+  transform: translate(0%, 0px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  white-space: nowrap;
+  word-spacing: -0.25em;
+  color: white;
+  font-size: 0.6em;
+  font-family: "Share Tech Mono", monospace;
+  letter-spacing: 2px;
+  text-transform: uppercase;
+  -webkit-font-smoothing: antialiased;
+  font-weight: 500;
 `
 
 const Container = styled.div`
-    -webkit-order: -1;
-    order: -1;
-    overflow: hidden;
-    color: #fff;
-    font-size: 1.3em;
-    font-family: "Share Tech Mono",monospace;
-    letter-spacing: 2px;
-    text-transform: uppercase;
-    background-image: linear-gradient(0deg,#4b4c50,#696969);
-    -webkit-font-smoothing: antialiased;
-    width: 200px;
-    padding: 0px 1.5em;
-    position relative;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 100%;
-    max-width: 250px;
+  -webkit-order: -1;
+  order: -1;
+  overflow: hidden;
+  color: #fff;
+  font-size: 1.3em;
+  font-family: "Share Tech Mono",monospace;
+  letter-spacing: 2px;
+  text-transform: uppercase;
+  background-image: linear-gradient(0deg,#4b4c50,#696969);
+  -webkit-font-smoothing: antialiased;
+  width: 175px;
+  padding: 0px 1.5em;
+  position relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  max-width: 250px;
 `
 
 const Timer = styled.div`
-  position: relative;
   height: 40px;
   display: flex;
   align-iterms: center;
 `
 
-const Clock = styled.div`
-  position: relative;
-  // margin-top: 150px;
-  z-index: 1;
-  box-sizing: border-box;
-  width: 200px;
-  height: 100%;
-  padding: 0 1.5em;
-  background-image: linear-gradient(180deg,#363636,#2b2b2b);
-  transform-origin: center 0;
-  transform: translate(0px, 0%);
-  height: 100%;
+const Date = styled.span`
   display: flex;
-`
-const Logo = styled.div`
-    width: 69px;
-    transform: translate(0%, -50%);
-    left: 0px;
-    position: absolute;
-    top: 50%;
-    z-index: 1;
-    display: flex;
-    justify-content: center;
-    overflow: hidden;
-    img {
-        transform: translate(0px, 0%);
-        width: 4.3em;
-        height: 100%;
-        transition: fill .3s ease;
-        filter: invert(100%);
-    }
-`
-
-const ClockWrapper = styled.div`
-    position: relative;
-    width: 100%;
-    height: 100%;
-    span{
-        opacity: 1;
-        visibility: inherit;
-        transform: translate(0%, -50%);
-        position: absolute;
-        right: 0;
-        top: 50%;
-        display: flex;
-        align-items: center;
-        margin-left: 0.4em;
-        color: white;
-        font-size: 1.7em;
-        font-family: 'Hanken Grotesk', sans-serif;
-        font-family: 'Quantico', sans-serif;
-        user-select: none;
-        display: flex;
-    }
+  font-size: 18px;
+  text-align: center;
+  margin-left: 100px;
 `
 
